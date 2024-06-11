180ed341e1ee5a0f3eb3668044a2d7d13fd61dc3
Joel Dominic Lobo <joeldominiclobo@Joels-MacBook-Air.local>
2024-06-11 20:34:59 +0530

    useDebounce hook

---
+++ src/hooks/useDebounce.jsx
@@ -0,0 +1,23 @@
+import {useEffect, useState} from 'react'
+
+const useDebounce = (value, delay = 500) => {
+    const[debouncedValue, setDebouncedValue] = useState(value)
+
+    useEffect(() => {
+        const id = setTimeout(() => {
+            console.log("setting new timeout")
+            setDebouncedValue(value)
+        }, delay)
+
+        return () => {
+            console.log("clearing the timeout")
+            clearTimeout(id)
+        }
+    })
+
+  return (
+    debouncedValue
+  )
+}
+
+export default useDebounce
